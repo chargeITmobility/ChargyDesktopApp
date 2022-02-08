0e653f0b615c113b13e5bbc7ac8bf6f5e40a185d
Christian Meusel <christian.meusel@posteo.de>
2022-02-08 17:23:01 +0100

    Build a whole bunch of targets by default

--- build.sh
+++ build.sh
@@ -4,4 +4,9 @@
 sass src/css/chargy.scss src/css/chargy.css
 webpack -c webpack.config.cjs
 
-electron-builder
+electron-builder --linux --x64
+electron-builder --macos --arm64
+electron-builder --macos --x64
+electron-builder --windows --arm64
+electron-builder --windows --x64
+
